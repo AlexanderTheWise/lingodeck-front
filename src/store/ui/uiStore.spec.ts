9f6722871712d208fa9cdf7e75e1aa6a7ad37ac9
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-04-27 22:15:46 +0200

    Test uiStore actions

---
+++ src/store/ui/uiStore.spec.ts
@@ -0,0 +1,73 @@
+import { describe, it } from "vitest";
+import { createPinia, setActivePinia } from "pinia";
+import useUiStore, { uiInitialState } from "./uiStore";
+import { modalPayload, openedModal } from "@/mocks/data";
+
+setActivePinia(createPinia());
+
+const uiStore = useUiStore();
+
+describe("openModal action", () => {
+  it("should update modal isError, title and message with modal payload and set isOpened to true", ({
+    expect,
+  }) => {
+    uiStore.openModal(modalPayload);
+
+    expect(uiStore.ui.modal).toStrictEqual(openedModal);
+  });
+});
+
+describe("closeModal action", () => {
+  it("should reset modal title, message and isOpened, and keep last isError value, in this case, true", ({
+    expect,
+  }) => {
+    expect(uiStore.ui.modal).toStrictEqual(openedModal);
+
+    uiStore.closeModal();
+
+    expect(uiStore.ui.modal).toStrictEqual({
+      ...uiInitialState.modal,
+      isError: modalPayload.isError,
+    });
+  });
+});
+
+describe("openEyes action", () => {
+  it("should set openEyes to true", ({ expect }) => {
+    expect(uiStore.ui.openEyes).toBe(false);
+
+    uiStore.openEyes();
+
+    expect(uiStore.ui.openEyes).toBe(true);
+  });
+});
+
+describe("closeEyes action", () => {
+  it("should set openEyes to false", ({ expect }) => {
+    expect(uiStore.ui.openEyes).toBe(true);
+
+    uiStore.closeEyes();
+
+    expect(uiStore.ui.openEyes).toBe(false);
+  });
+});
+
+describe("setLoading action", () => {
+  it("should set isLoading to true", ({ expect }) => {
+    expect(uiStore.ui.isLoading).toBe(false);
+
+    uiStore.setLoading();
+
+    expect(uiStore.ui.isLoading).toBe(true);
+  });
+});
+
+describe("unsetLoading action", () => {
+  it("should set isLoading to false", ({ expect }) => {
+    expect(uiStore.ui.isLoading).toBe(true);
+
+    uiStore.unsetLoading();
+
+    expect(uiStore.ui.isLoading).toBe(false);
+  });
+});
