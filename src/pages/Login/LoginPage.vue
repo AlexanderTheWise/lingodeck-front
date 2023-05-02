2fa41237ef5aaa9fcbfc70cd870190483743484d
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-05-02 18:44:29 +0200

    Add LoginPage component

---
+++ src/pages/Login/LoginPage.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import CredentialsForm from "@/components/CredentialsForm/CredentialsForm.vue";
+import FormEyes from "@/components/FormEyes/FormEyes.vue";
+import userServices from "@/services/user/userServices";
+
+const { loginUser } = userServices();
+</script>
+
+<template>
+  <div class="credentials-page box-column">
+    <div class="credentials-page__eyes-form">
+      <FormEyes />
+      <CredentialsForm @submit="loginUser"> Log in </CredentialsForm>
+    </div>
+    <span class="credentials-page__redirection"
+      >Doesn't have an account? Sign up!</span
+    >
+  </div>
+</template>
+
+<style scoped lang="scss">
+.credentials-page {
+  gap: 26px;
+
+  &__eyes-form {
+    background: linear-gradient(to bottom, #0080ff, #010c80, #ffffff);
+  }
+
+  &__redirection {
+    font-weight: 700;
+    background-color: white;
+    text-align: center;
+  }
+}
+</style>
