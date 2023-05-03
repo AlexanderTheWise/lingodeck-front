4f0b9ce652f380a415e020315174d31ee86a5a95
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-05-03 23:14:01 +0200

    Add Modal component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
 import Loader from "./components/Loader/Loader.vue";
+import ModalVue from "./components/Modal/Modal.vue";
 </script>
 
 <template>
+  <ModalVue />
   <main class="box-row">
     <Loader />
   </main>
 </template>
-
-<style scoped>
-main {
-  justify-content: center;
-}
-</style>
