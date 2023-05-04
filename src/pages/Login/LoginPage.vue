4deffd515f1cbc3edc139270cf0b1e9340fd3fa7
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-05-04 21:14:53 +0200

    Abstract credentials pages styles to one scss file

--- src/pages/Login/LoginPage.vue
+++ src/pages/Login/LoginPage.vue
@@ -10,26 +10,13 @@ const { loginUser } = userServices();
   <div class="credentials-page box-column">
     <div class="credentials-page__eyes-form">
       <FormEyes />
-      <CredentialsForm @submit="loginUser"> Log in </CredentialsForm>
+      <CredentialsForm @submit="loginUser">Log in</CredentialsForm>
     </div>
     <span class="credentials-page__redirection"
-      >Doesn't have an account? Sign up!</span
+      >Doesn't have an account?
+      <RouterLink class="credentials-page__link" :to="{ name: 'Register' }"
+        >Sign up</RouterLink
+      >!</span
     >
   </div>
 </template>
-
-<style scoped lang="scss">
-.credentials-page {
-  gap: 26px;
-
-  &__eyes-form {
-    background: linear-gradient(to bottom, #0080ff, #010c80, #ffffff);
-  }
-
-  &__redirection {
-    font-weight: 700;
-    background-color: white;
-    text-align: center;
-  }
-}
-</style>
