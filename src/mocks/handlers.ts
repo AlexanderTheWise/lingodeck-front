5f0034f9170f45be31785b18276af2a1490215c5
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-05-04 19:13:21 +0200

    Test registerUser service

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -14,6 +14,19 @@ const handlers = [
     if (areCorrect) return res(ctx.json({ token: mockUser.token }));
     return res(ctx.status(400));
   }),
+  rest.post(`${lingodeckBack}/user/register`, async (req, res, ctx) => {
+    const credentials = await req.json<UserCredentials>();
+
+    if (
+      Object.values(credentials).every((credential: string) =>
+        /^[a-zA-Z0-9]{8,24}$/.test(credential)
+      )
+    ) {
+      return res(ctx.status(201));
+    }
+
+    return res(ctx.status(400));
+  }),
 ];
 
 export default handlers;
