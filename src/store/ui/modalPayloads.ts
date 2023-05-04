4b4ec2bf1fc0ae384d0211c0a2ed03fe8fc12769
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-05-04 19:10:58 +0200

    Add registerConfirm and registerError keys

--- src/store/ui/modalPayloads.ts
+++ src/store/ui/modalPayloads.ts
@@ -1,14 +1,28 @@
 import type { ModalPayload } from "@/types";
 
+type ModalPayloadSet = Record<string, ModalPayload>;
 interface ModalPayloads {
-  errors: Record<string, ModalPayload>;
+  confirm: ModalPayloadSet;
+  errors: ModalPayloadSet;
 }
 
 const modalPayloads: ModalPayloads = {
+  confirm: {
+    registerConfirm: {
+      isError: false,
+      title: "Successfully registered",
+      message: "You can login now",
+    },
+  },
   errors: {
     loginError: {
       isError: true,
       title: "Couldn't login",
+      message: "Please, try again",
+    },
+    registerError: {
+      isError: true,
+      title: "Couldn't register",
       message: "Please, try again",
     },
   },
