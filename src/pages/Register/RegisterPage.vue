6bad2c5fcbc398c5f7749ae8ec854f6aab4c5e8a
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-05-04 21:20:00 +0200

    Add RegisterPage component

---
+++ src/pages/Register/RegisterPage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+import FormEyes from "@/components/FormEyes/FormEyes.vue";
+import CredentialsForm from "@/components/CredentialsForm/CredentialsForm.vue";
+import userServices from "@/services/user/userServices";
+
+const { registerUser } = userServices();
+</script>
+
+<template>
+  <div class="credentials-page box-column">
+    <div class="credentials-page__eyes-form">
+      <FormEyes />
+      <CredentialsForm @submit="registerUser">Sign up</CredentialsForm>
+    </div>
+    <span class="credentials-page__redirection"
+      >Already have an account?
+      <RouterLink class="credentials-page__link" :to="{ name: 'Log in' }"
+        >Log in</RouterLink
+      >!</span
+    >
+  </div>
+</template>
