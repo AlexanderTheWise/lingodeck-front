f720119c1183796154d8b781bc8067c710098375
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-05-08 15:50:32 +0200

    Add createFlashcard, deleteFlashcard and modifyFlashcard

--- src/services/flashcards/flashcardsServices.ts
+++ src/services/flashcards/flashcardsServices.ts
@@ -2,7 +2,13 @@ import useFlashcardsStore from "@/store/flashcard/flashcardStore";
 import modalPayloads from "@/store/ui/modalPayloads";
 import useUiStore from "@/store/ui/uiStore";
 import useUserStore from "@/store/user/userStore";
-import type { Flashcards } from "@/types";
+import type {
+  FlashcardsResponse,
+  Flashcards,
+  NewOrModifiedCard,
+  FlashcardResponse,
+} from "@/types";
+import { useRouter } from "vue-router";
 
 interface FlashcardsServices {
   getFlashcards: (
@@ -11,13 +17,21 @@ interface FlashcardsServices {
     language: string,
     shouldReturn: boolean
   ) => Promise<void | Flashcards>;
+  createFlashcard: (createdCard: NewOrModifiedCard) => Promise<void>;
+  modifyFlashcard: (
+    modifiedCard: NewOrModifiedCard,
+    id: string
+  ) => Promise<void>;
+  deleteFlashcard: (id: string) => Promise<void>;
 }
 
 const lingodeckBack: string = import.meta.env.VITE_LINGODECK_BACK;
+const isTesting = process.env.NODE_ENV === "test";
 
 const flashcardsServices = (): FlashcardsServices => {
   const flashcardsStore = useFlashcardsStore();
   const uiStore = useUiStore();
+  const router = useRouter();
   const { token } = useUserStore().user;
 
   const getFlashcards = async (
@@ -40,9 +54,9 @@ const flashcardsServices = (): FlashcardsServices => {
 
       if (!(await response).ok) throw new Error();
 
-      const { flashcards } = (await (await response).json()) as {
-        flashcards: Flashcards;
-      };
+      const { flashcards } = (await (
+        await response
+      ).json()) as FlashcardsResponse;
 
       if (shouldReturn) {
         uiStore.unsetLoading();
@@ -58,7 +72,100 @@ const flashcardsServices = (): FlashcardsServices => {
     }
   };
 
-  return { getFlashcards };
+  const createFlashcard = async (createdCard: NewOrModifiedCard) => {
+    try {
+      uiStore.setLoading();
+      const body = new FormData();
+
+      Object.keys(createdCard).forEach((key) =>
+        body.append(key, createdCard[key as keyof NewOrModifiedCard]!)
+      );
+
+      const response = await fetch(`${lingodeckBack}/flashcards`, {
+        body: isTesting ? JSON.stringify(createdCard) : body,
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) throw new Error();
+
+      const { flashcard } = (await (
+        await response
+      ).json()) as FlashcardResponse;
+
+      flashcardsStore.addFlashcard({ ...flashcard });
+      router.push({ name: "Flashcards" });
+      uiStore.unsetLoading();
+      uiStore.openModal(modalPayloads.confirm.createConfirm);
+    } catch (error) {
+      uiStore.unsetLoading();
+
+      uiStore.openModal(modalPayloads.errors.createError);
+    }
+  };
+
+  const modifyFlashcard = async (
+    modifiedCard: NewOrModifiedCard,
+    id: string
+  ) => {
+    try {
+      uiStore.setLoading();
+      const body = new FormData();
+
+      Object.keys(modifiedCard).forEach((key) =>
+        body.append(key, modifiedCard[key as keyof NewOrModifiedCard]!)
+      );
+
+      const response = await fetch(`${lingodeckBack}/flashcards/${id}`, {
+        body: isTesting ? JSON.stringify(modifiedCard) : body,
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) throw new Error();
+
+      const { flashcard } = (await (
+        await response
+      ).json()) as FlashcardResponse;
+
+      flashcardsStore.modifyFlashcard(id, { ...flashcard });
+      router.push({ name: "Flashcards" });
+      uiStore.unsetLoading();
+      uiStore.openModal(modalPayloads.confirm.modifyConfirm);
+    } catch (error) {
+      uiStore.unsetLoading();
+
+      uiStore.openModal(modalPayloads.errors.modifyError);
+    }
+  };
+
+  const deleteFlashcard = async (id: string) => {
+    try {
+      uiStore.setLoading();
+      const response = await fetch(`${lingodeckBack}/flashcards/${id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) throw new Error();
+
+      flashcardsStore.deleteFlashcard(id);
+      uiStore.unsetLoading();
+      uiStore.openModal(modalPayloads.confirm.deleteConfirm);
+    } catch (error) {
+      uiStore.unsetLoading();
+
+      uiStore.openModal(modalPayloads.errors.deleteError);
+    }
+  };
+
+  return { getFlashcards, createFlashcard, modifyFlashcard, deleteFlashcard };
 };
 
 export default flashcardsServices;
